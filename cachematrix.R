b0f899af0de070e2b9b4ab219e3ef4d342f779cb
beethovensaves <fecasadei@gmail.com>
2014-06-18 21:49:42 -0700

    Reply
    Replied to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Creates a matrix listing a function that
+## a) sets matrix value
+## b) gets matrix value
+## c) sets inverse matrix value
+## d) gets inverse matrix value
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    ## set stored inverse value to null
+    inverse.value <- null
+    
+    ## set matrix value
+    
+    matrix.value <- function(y) {
+        x <<- y
+        inverse.value <<- null
+    }
+    ## pulling value from matrix
+    pull.matrix <- function() x
+    ## setting inverse value
+    set.inverse <- function(inv_) inverse.value <<- inv_
+    ## pulling inverse value
+    pull.inverse <- function () inverse.value
+    
+    ## print a list of all above functions
+    list(matrix.value = matrix.value, pull.matrix = pull.matrix, set.inverse = set.inverse, pull.inverse = pull.inverse)
 
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of above matrix, first verifying if calculation has done.
+## If calculation is done, pulls inverse from cache.
+## Else calculates the inverse of the matrix and sets value
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Verification
+        inverse.value <- x$pull.inverse()
+        if(!is.null(inverse.value)) {
+            message("pulling cached data")
+            return(inverse.value)
+        }
+        ##if not cached
+        data <- x$pull.matrix()
+        ## calculate inverse
+        inverse.value <- solve(data, ...)
+        ## then cache the inverse and print
+        x$set.inverse(inverse.value)
+        inverse.value
 }
